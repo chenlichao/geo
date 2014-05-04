b16e011b445e5212d52818a46839eb5c251bec92
Lichao Chen <lichao@earth>
2014-05-04 15:47:46 -0700

    fixed scale calculation, added x,y calculation and noise discovery

--- contour.cpp
+++ contour.cpp
@@ -6,6 +6,7 @@
 #define _DEBUG
 #include <map>
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <graph.hpp>
 #include "boost/accumulators/accumulators.hpp"
@@ -29,13 +30,21 @@ int main(int argc, char** argv)
 	string ifname;
 	string tfname;
 	string ofname;
+	string oxfname;
+	string oyfname;
+	string onfname;
+	string config;
 
 	po::options_description desc("General Options");
 	desc.add_options()
 		("help", "produce help message")
+		("config,C",po::value<string>(&config),"configuration file")
 		("input,I",po::value<string>(&ifname),"input file")
 		("table,T",po::value<string>(&tfname),"lookup table file")
-		("output,O",po::value<string>(&ofname),"output file");
+		("output,O",po::value<string>(&ofname),"output file")
+		("xoutput,X",po::value<string>(&oxfname),"x output file")
+		("youtput,Y",po::value<string>(&oyfname),"y output file")
+		("noutput,N",po::value<string>(&onfname),"isnoisy output file");
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -44,7 +53,14 @@ int main(int argc, char** argv)
 	if(vm.count("help")) {
 		cout<<desc<<"\n";
 		return 0;
+	} else if (vm.count("config")) {
+		cout << config << endl;
+		ifstream fconf(config);
+		po::store(po::parse_config_file(fconf, desc), vm);
+		po::notify(vm);
+		fconf.close();
 	}
+
 	auto tabmx = map<int,float>();
 	auto tabmy = map<int,float>();
 	auto tabmr = map<int,float>();
@@ -59,6 +75,9 @@ int main(int argc, char** argv)
 	FILE *fin = fopen(ifname.c_str(),"r");
 	FILE *ftable = fopen(tfname.c_str(),"r");
 	FILE *fout=fopen(ofname.c_str(),"w");
+	FILE *foutx=fopen(oxfname.c_str(),"w");
+	FILE *fouty=fopen(oyfname.c_str(),"w");
+	FILE *foutn=fopen(onfname.c_str(),"w");
 	int i = 1;
 	int ct;
 
@@ -76,6 +95,7 @@ int main(int argc, char** argv)
 	fclose(ftable);
 	cout<<"there are "<<i<<" entries."<<endl;
 
+	//Load graph
 	auto g = graph();
 	while (fscanf(fin, "%d\t%d", &from,&to)!=EOF) {
 		g.addNode(from);
@@ -108,6 +128,9 @@ int main(int argc, char** argv)
 	set<int> visited;
 	set<int> queue;
 	map<int,vector<float>> result;
+	map<int,vector<float>> resultx;
+	map<int,vector<float>> resulty;
+	map<int,vector<bool>> resultn;
 
 	queue.insert(nids[0]);
 	while(queue.size()>0){
@@ -116,6 +139,8 @@ int main(int argc, char** argv)
 		queue.erase(queue.begin());
 		if (current==nids[0]){
 			g.nodes[nids[0]].scale=1;
+			g.nodes[nids[0]].absx=0;
+			g.nodes[nids[0]].absy=0;
 		}else { 
 			g.nodes[current].calculateScale(g.nodes);
 		}
@@ -131,8 +156,11 @@ int main(int argc, char** argv)
 
 	for(auto it=g.nodes.begin();it!=g.nodes.end();it++){
 		result[it->first].push_back(it->second.scale);
+		resultx[it->first].push_back(it->second.absx);
+		resulty[it->first].push_back(it->second.absy);
+		resultn[it->first].push_back(it->second.noise);
 	}
-	for(int j=0;j<5;j++){
+	for(int j=0;j<10;j++){
 		for(auto it=nids.begin();it!=nids.end();it++){
 			if(g.nodes[*it].scale!=0){
 				g.nodes[*it].calculateScale(g.nodes);
@@ -140,6 +168,9 @@ int main(int argc, char** argv)
 		}
 		for(auto it=g.nodes.begin();it!=g.nodes.end();it++){
 			result[it->first].push_back(it->second.scale);
+			resultx[it->first].push_back(it->second.absx);
+			resulty[it->first].push_back(it->second.absy);
+			resultn[it->first].push_back(it->second.noise);
 		}
 
 	}
@@ -152,8 +183,33 @@ int main(int argc, char** argv)
 		fprintf(fout,"\n");
 	}
 
+	for(auto it=resultx.begin();it!=resultx.end();it++){
+		fprintf(foutx,"%d",it->first);
+		for(auto it2=it->second.begin();it2!=it->second.end();it2++){
+			fprintf(foutx,"\t%f",*it2);
+		}
+		fprintf(foutx,"\n");
+	}
+	for(auto it=resulty.begin();it!=resulty.end();it++){
+		fprintf(fouty,"%d",it->first);
+		for(auto it2=it->second.begin();it2!=it->second.end();it2++){
+			fprintf(fouty,"\t%f",*it2);
+		}
+		fprintf(fouty,"\n");
+	}
+	for(auto it=resultn.begin();it!=resultn.end();it++){
+		fprintf(foutn,"%d",it->first);
+		for(auto it2=it->second.begin();it2!=it->second.end();it2++){
+			fprintf(foutn,"\t%s",(*it2)?"true":"false");
+		}
+		fprintf(foutn,"\n");
+	}
+
 	cout << "there are "<<g.nodes.size()<<" node."<<endl;
 	fclose(fin);
 	fclose(fout);
+	fclose(foutx);
+	fclose(fouty);
+	fclose(foutn);
 	cout << "done." << endl;
 }
--- include/graph.hpp
+++ include/graph.hpp
@@ -1,4 +1,6 @@
 #include<set>
+#include<limits>
+#include<map>
 #include<algorithm>
 
 
@@ -52,11 +54,36 @@ class node {
 					return nodes[i].scale!=0;
 					});
 			float s=0;
+			float x=0;
+			float y=0;
+			float maxs=numeric_limits<float>::min();//or lowest() in c++11?
+			float mins=numeric_limits<float>::max();
+
 			for(auto it=assigned.begin();it!=assigned.end();it++){
-				s+=nodes[*it].edges[id].mr * nodes[*it].scale;
+				float ts =nodes[*it].edges[id].mr * nodes[*it].scale; 
+				s+=ts;
+				x+=nodes[*it].edges[id].mx * nodes[*it].scale + nodes[*it].absx;
+				y+=nodes[*it].edges[id].my * nodes[*it].scale + nodes[*it].absy;
+				if (ts>maxs)
+				{
+					maxs=ts;
+				}
+				if (ts<mins)
+				{
+					mins =ts;
+				}		
 			}
 			s/=assigned.size();
+			x/=assigned.size();
+			y/=assigned.size();
 			scale = s;
+			absx = x;
+			absy = y;
+			if (maxs-mins>0.1){
+				noise = true;
+			}else{
+				noise = false;
+			}
 #ifdef _DEBUG
 			cout<<"nodes "<<id<<" scale assigned: "<<s<<endl;
 #endif
@@ -64,7 +91,10 @@ class node {
 	public:
 		map<int, edge> edges;
 		set<int> neighbors;
-		float scale;
+		float scale=0;
+		float absx=0;
+		float absy=0;
+		bool noise=true;
 		int id;
 };
 
